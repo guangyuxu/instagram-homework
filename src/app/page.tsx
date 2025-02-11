f0f5d96bf01f4619094b440b04a3b13f5150998e
guangyuxu <guangyuxu@gmail.com>
2025-02-10 23:19:10 -0800

    remove quota

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import PhotoGrid from "../components/PhotoGrid";
 export default function Home() {
     return (
         <div className="max-w-[602px] mx-auto p-4">
-            <h1 className="text-3xl font-bold text-center mb-6">Gavin's photos</h1>
+            <h1 className="text-3xl font-bold text-center mb-6">Gavin Xu Homework</h1>
             <Profile profileImage="https://picsum.photos/100" username="#GavinXu" postCount={10690626}/>
             <h3 className="text-gray-600 text-sm font-medium ml-2 mb-4">Top posts</h3>
             <PhotoGrid/>
